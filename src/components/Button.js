f0c9fbfcacf7183bb5e42a6203bb309f6fcdf6e2
ChrisRossRokita <88979453+ChrisRossRokita@users.noreply.github.com>
2021-08-18 11:27:49 -0400

    Navbar Commit

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,16 +1,16 @@
-import React, { Component } from 'react';
+// import React, { Component } from 'react';
 import './Button.css';
 import { Link } from 'react-router-dom';
-import { mediumaquamarine } from 'color-name';
+// import { mediumaquamarine } from 'color-name';
 
 const STYLES = ['btn--primary', 'btn--outline'];
 
 const SIZES = ['btn--medium', 'btn--large'];
 
 export const Button = ({children, type, onClick, buttonStyle, buttonSize}) => {
-    const checkButtonStyle = STYLES.include(bottonStyle) ? buttonStyle : STYLES[0];
+    const checkButtonStyle = STYLES.includes(buttonStyle) ? buttonStyle : STYLES[0];
 
-const checkButtonSize = SIZES.includes(buttonSize) ? buttonSize : Sizes[0]
+const checkButtonSize = SIZES.includes(buttonSize) ? buttonSize : SIZES[0]
 return (
     <Link to='/sign-up' className='btn-mobile'>
         <button
